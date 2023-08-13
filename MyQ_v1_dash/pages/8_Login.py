f70419823f09796a65a1d5f3a4ca7f6a60d00e7d
myquantfolio <103963768+ryansilk@users.noreply.github.com>
2023-08-13 17:30:11 +0100

    Update 8_Login.py

--- MyQ_v1_dash/pages/8_Login.py
+++ MyQ_v1_dash/pages/8_Login.py
@@ -16,7 +16,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 def display_subscription_message():
 
-    url = 'https://buy.stripe.com/6oEaHAaZG6s0a4g4gg'
+    url = 'https://buy.stripe.com/7sIdRK4ti85E2paeUU'
 
     st.markdown(
         f'''
