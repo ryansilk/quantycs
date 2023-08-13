4eeac36383a91158dcc5e0bb234acb67aa48b4ee
myquantfolio <103963768+ryansilk@users.noreply.github.com>
2023-08-13 12:23:00 +0100

    Update Dashboard.py

--- MyQ_v1_dash/Dashboard.py
+++ MyQ_v1_dash/Dashboard.py
@@ -631,7 +631,7 @@ else:
         with c1:
 
             # Load the Excel file
-            excel_file = '/Users/ryansilk/PycharmProjects/MyQ_v1_dash/pages/Stock_List.xlsx'
+            excel_file = 'MyQ_v1_dash/pages/Stock_List.xlsx'
             df = pd.read_excel(excel_file)
 
             options = df['symbol'].tolist()  # Replace 'Column_Name' with the actual column name from your Excel sheet
