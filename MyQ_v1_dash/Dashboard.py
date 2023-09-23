ec2a7a8a12f61e7e88595b06f080a423514da4b1
myquantfolio <103963768+ryansilk@users.noreply.github.com>
2023-09-23 10:51:10 +0100

    Update Dashboard.py

--- MyQ_v1_dash/Dashboard.py
+++ MyQ_v1_dash/Dashboard.py
@@ -34,7 +34,7 @@ def display_subscription_message():
         unsafe_allow_html=True
     )
 import pyEX as p
-token = 'sk_17ccaec85d7943a78c9269f9e17f664e'
+token = 'pk_a54d1f90eb574b2d978d86b6454bef3e'
 
 st.set_page_config(layout="wide")
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: left;} </style>', unsafe_allow_html=True)
